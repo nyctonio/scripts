d890ec38d27c069716b8be37cfe22e0c2f929cf2
Ritesh Kumar <nyctonio.dev@gmail.com>
2022-09-25 18:09:41 +0530

    Create nodejs.sh

---
+++ nodejs.sh
@@ -0,0 +1,10 @@
+sudo adduser nio
+sudo usermod -a -G sudo nio
+su - nio
+sudo apt-get update
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+nvm install --lts
+npm install -g pnpm
+sudo npm i pm2 -g
+sudo apt install nginx
+sudo apt-get install python3-certbot-nginx
