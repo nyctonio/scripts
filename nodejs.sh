f2e99befe5f959d47892c797371f994408ba9c41
Ritesh Kumar <nyctonio.dev@gmail.com>
2022-09-25 18:35:24 +0530

    Update nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -1,7 +1,6 @@
 sudo apt update && sudo apt install curl -y
 sudo curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 exec bash
-source ~/.profile
 nvm install --lts
 npm install -g pnpm
 sudo npm i pm2 -g
