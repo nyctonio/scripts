2d09f3de975c161c58e4323d990de1af81930ff0
Ritesh Kumar <nyctonio.dev@gmail.com>
2022-09-25 18:50:47 +0530

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,3 +1,5 @@
 sudo apt update && sudo apt install curl -y
+sudo apt install nginx
+sudo apt-get install python3-certbot-nginx
 sudo curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 exec bash
