dc74175e7baaea289121809c82165a44c636e3a8
Ritesh Kumar <nyctonio.dev@gmail.com>
2022-09-25 18:40:14 +0530

    Update and rename nodejs.sh to preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -1,8 +1,3 @@
 sudo apt update && sudo apt install curl -y
 sudo curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 exec bash
-nvm install --lts
-npm install -g pnpm
-sudo npm i pm2 -g
-sudo apt install nginx
-sudo apt-get install python3-certbot-nginx
