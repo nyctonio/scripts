58bd10b6c5fe5352b64953ecbce55870074bcf3a
Ritesh Kumar <nyctonio.dev@gmail.com>
2023-08-22 23:21:50 +0530

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -3,4 +3,7 @@ sudo apt install nginx
 sudo apt-get install python3-certbot-nginx
 sudo apt install nodejs
 sudo apt install npm
-npm install -g n
+sudo npm install -g n
+sudo n lts
+sudo npm i -g pnpm
+
